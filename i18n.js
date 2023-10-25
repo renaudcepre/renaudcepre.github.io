12ba663dcfeeb82b077312ab51dcabccd95fe5d0
rcepre <renaudcepre@gmail.com>
2023-10-25 14:10:52 +0200

    First test with i18n

---
+++ i18n.js
@@ -0,0 +1,57 @@
+
+function applyFadeEffect(element, callback) {
+    element.classList.add('fade-out');
+
+    setTimeout(() => {
+        // Appliquer la mise à jour du texte ou autres choses
+        callback();
+
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+
+        setTimeout(() => {
+            element.classList.remove('fade-in');
+        }, 100);
+    }, 100);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let translationData = {};
+
+    let browserLang = navigator.language.substr(0, 2).toLowerCase();
+    let currentLang = ['fr', 'en'].includes(browserLang) ? browserLang : 'en';
+
+    const btnToggle = document.getElementById('toggle-translate');  // Déplacé ici
+
+function translateText(language) {
+    document.querySelectorAll('[data-translate-key]').forEach(element => {
+        let key = element.getAttribute('data-translate-key');
+        let translatedText = translationData[key][language];
+        if (translatedText) {
+            applyFadeEffect(element, () => {
+                element.textContent = translatedText;
+            });
+        }
+    });
+}
+
+
+    function updateButtonText() {
+        btnToggle.textContent = (currentLang === 'fr') ? 'EN' : 'FR';
+    }
+
+    fetch('i18n.json')
+        .then(response => response.json())
+        .then(data => {
+            translationData = data;
+            translateText(currentLang);
+            updateButtonText();
+        })
+        .catch(error => console.error('Erreur lors de la récupération du fichier i18n.json:', error));
+
+    btnToggle.addEventListener('click', function () {
+        currentLang = (currentLang === 'fr') ? 'en' : 'fr';
+        translateText(currentLang);
+        updateButtonText();
+    });
+});
