dbfc9cf78161b34e0ca017d1f86cf1eb237c4abc
rcepre <renaudcepre@gmail.com>
2023-10-26 12:50:35 +0200

    Remove useless comments

--- email.js
+++ email.js
@@ -1,29 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // ... Votre code existant ...
-
-    // Gestion de la boîte modale
     const modal = document.getElementById('emailModal');
     const btn = document.getElementById('showEmail');
     const span = document.getElementById('closeModal');
     const copyBtn = document.getElementById('copyEmail');
-    const mainContent = document.querySelector('.main-content');  // Obtenir l'élément avec la classe "main-content"
+    const mainContent = document.querySelector('.main-content');
 
-    // Quand l'utilisateur clique sur le bouton, ouvre la boîte modale
+
     btn.addEventListener('click', function () {
-        mainContent.classList.add('blur');  // Ajoute l'effet de flou à main-content
+        mainContent.classList.add('blur');
         modal.style.display = "block";
     });
 
-// Fonction pour fermer la boîte modale
     function closeModal() {
         mainContent.classList.remove('blur');
         modal.style.display = "none";
     }
 
-// Lorsque l'utilisateur clique sur (x)
     span.addEventListener('click', closeModal);
 
-// Lorsque l'utilisateur appuie sur la touche "Escape"
     document.addEventListener('keydown', function (event) {
         if (event.key === 'Escape') {
             closeModal();
@@ -31,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-    // Quand l'utilisateur clique sur "Copier", copie l'email dans le presse-papier
     copyBtn.addEventListener('click', function () {
         navigator.clipboard.writeText('renaudcepre-dev@gmail.com').then(() => {
             copyBtn.innerHTML = "👌"
